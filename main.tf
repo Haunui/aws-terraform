ba068267e0199135d38070d567446fad5e2d240f
Haunui Saint-sevin <haunui.saint-sevin@naos-cluster.tech>
2022-07-21 23:01:36 +0200

    Infra. ok

--- main.tf
+++ main.tf
@@ -54,16 +54,37 @@ resource "aws_instance" "instance0" {
   subnet_id = each.value.subnet_id
   vpc_security_group_ids = each.value.vpc_security_group_ids
   associate_public_ip_address = each.value.associate_public_ip_address
+  private_ip = each.value.private_ip
+
+  user_data = fileexists(try("${each.value.user_data}", "does_not_exist")) ? file("${each.value.user_data}") : null
 
   tags = {
     Name = "${var.prefix.instance}haunui-${each.key}"
   }
 
   provisioner "local-exec" {
-    command = "echo ${self.public_ip} > public_ip"
+    command = "mkdir -p instances; echo ${self.public_ip} > instances/${each.key}-public_ip"
+  }
+
+  provisioner "local-exec" {
+    command = "mkdir -p instances; echo ${self.private_ip} > instances/${each.key}-private_ip"
   }
 }
 
+resource "aws_default_route_table" "drtb0" {
+  for_each = local.drtb
+  default_route_table_id = each.value.default_route_table_id
+  
+  dynamic "route" {
+    for_each = toset(each.value.routes)
+
+    content {
+      cidr_block = route.value.cidr_block
+      vpc_peering_connection_id = route.value.vpc_peering_connection_id
+    }
+  }
+}
+ 
 resource "aws_route_table" "rtb0" {
   for_each = local.rtb
   vpc_id = each.value.vpc_id
@@ -83,7 +104,10 @@ resource "aws_route" "route0" {
   for_each = local.route
   route_table_id = "${each.value.route_table_id}"
   destination_cidr_block = "${each.value.destination_cidr_block}"
-  gateway_id = "${each.value.gateway_id}"
+  gateway_id = try("${each.value.gateway_id}", null)
+  nat_gateway_id = try("${each.value.nat_gateway_id}", null)
+  vpc_peering_connection_id = try("${each.value.vpc_peering_connection_id}", null)
+
 }
 
 resource "aws_eip" "eip0" {
@@ -101,6 +125,9 @@ resource "aws_security_group" "sg0" {
   description = each.value.description
   vpc_id = each.value.vpc_id
 
+  tags = {
+    Name = "${var.prefix.sg}haunui-${each.key}"
+  }
 }
 
 resource "aws_security_group_rule" "sgr0" {
@@ -112,4 +139,39 @@ resource "aws_security_group_rule" "sgr0" {
   to_port = each.value.to_port
   protocol = "${each.value.protocol}"
   cidr_blocks = try("${each.value.cidr_blocks}", null)
+}
+
+resource "aws_vpc_peering_connection" "vpc_pc0" {
+  for_each = local.vpc_pc
+  peer_owner_id = each.value.peer_owner_id
+  peer_vpc_id   = each.value.peer_vpc_id
+  vpc_id        = each.value.vpc_id
+  peer_region   = try(each.value.peer_region, null)
+  auto_accept   = true
+
+  tags = {
+    Name = "${var.prefix.vpc_pc}haunui-${each.key}"
+  }
+}
+
+resource "aws_network_acl" "nacl0" {
+  for_each = local.nacl
+  vpc_id = each.value.vpc_id
+  subnet_ids = try(each.value.subnet_ids, null)
+
+  tags = {
+    Name = "${var.prefix.nacl}haunui-${each.key}"
+  }
+}
+
+resource "aws_network_acl_rule" "nacl_rule0" {
+  for_each = local.nacl_rule
+  network_acl_id = each.value.network_acl_id
+  rule_number = each.value.rule_number
+  protocol = each.value.protocol
+  egress = try(each.value.egress, null)
+  rule_action = each.value.rule_action
+  cidr_block = each.value.cidr_block
+  from_port = try(each.value.from_port, null)
+  to_port = try(each.value.to_port, null)
 }
--- variables.tf
+++ variables.tf
@@ -10,13 +10,18 @@ variable "prefix" {
     rtb = "rtb-"
     eip = "eip-"
     sg = "sg-"
+    vpc_pc = "vpc_pc-"
+    nacl = "nacl-"
   }
 }
 
 variable "vpc" {
   type = any
   default = {
-    vpc0 = {
+    central = {
+      cidr_block = "172.22.0.0/16"
+    }
+    vpc1 = {
       cidr_block = "10.0.0.0/16"
     }
   }
@@ -24,111 +29,253 @@ variable "vpc" {
 
 locals {
   subnet = {
-    subnet0 = {
-      vpc_id = aws_vpc.vpc0["vpc0"].id
-      cidr_block = "10.0.1.0/24"
+    DMZ = {
+      vpc_id = aws_vpc.vpc0["central"].id
+      cidr_block = "172.22.0.0/24"
     }
-    subnet1 = {
-      vpc_id = aws_vpc.vpc0["vpc0"].id
-      cidr_block = "10.0.2.0/24"
+    IB = {
+      vpc_id = aws_vpc.vpc0["central"].id
+      cidr_block = "172.22.1.0/24"
+    }
+
+    vpc1sub0 = {
+      vpc_id = aws_vpc.vpc0["vpc1"].id
+      cidr_block = "10.0.0.0/24"
     }
   }
   igw = {
     igw0 = {
-      vpc_id = aws_vpc.vpc0["vpc0"].id
+      vpc_id = aws_vpc.vpc0["central"].id
     }
   }
-  nat = {
-    nat0 = {
-      connectivity_type = "public"
-      subnet_id = aws_subnet.subnet0["subnet0"].id
-      allocation_id = aws_eip.eip0["eip0"].id
-    }
-  }
+  nat = {}
   instance = {
-    instance0 = {
+    admin = {
       ami = "ami-09e513e9eacab10c1"
       key_name = "keypair-haunui"
-      subnet_id = aws_subnet.subnet0["subnet0"].id
-      vpc_security_group_ids = ["${aws_security_group.sg0["sg0"].id}"]
+      subnet_id = aws_subnet.subnet0["DMZ"].id
+      vpc_security_group_ids = ["${aws_security_group.sg0["central"].id}"]
       associate_public_ip_address = true
+      user_data = "scripts/user_data.txt.template"
+      private_ip = "172.22.0.100"
     }
-    instance1 = {
+    proxy = {
       ami = "ami-09e513e9eacab10c1"
       key_name = "keypair-haunui"
-      subnet_id = aws_subnet.subnet0["subnet1"].id
-      vpc_security_group_ids = ["${aws_security_group.sg0["sg1"].id}"]
+      subnet_id = aws_subnet.subnet0["DMZ"].id
+      vpc_security_group_ids = ["${aws_security_group.sg0["central"].id}"]
+      associate_public_ip_address = true
+      user_data = "scripts/user_data.txt.template"
+      private_ip = "172.22.0.10"
+    }
+    ldapserver = {
+      ami = "ami-09e513e9eacab10c1"
+      key_name = "keypair-haunui"
+      subnet_id = aws_subnet.subnet0["IB"].id
+      vpc_security_group_ids = ["${aws_security_group.sg0["central"].id}"]
       associate_public_ip_address = false
+      private_ip = "172.22.1.20"
+    }
+
+    i01 = {
+      ami = "ami-09e513e9eacab10c1"
+      key_name = "keypair-haunui"
+      subnet_id = aws_subnet.subnet0["vpc1sub0"].id
+      vpc_security_group_ids = ["${aws_security_group.sg0["vpc1"].id}"]
+      associate_public_ip_address = false
+      private_ip = "10.0.0.200"
+    }
+  }
+
+  drtb = {
+    vpc1 = {
+      default_route_table_id = aws_vpc.vpc0["vpc1"].default_route_table_id
+
+      routes = [
+        {
+          cidr_block = "0.0.0.0/0"
+          vpc_peering_connection_id = aws_vpc_peering_connection.vpc_pc0["central_vpc1"].id
+        }
+      ]
     }
   }
 
   rtb = {
-    rtb0 = {
-      vpc_id = aws_vpc.vpc0["vpc0"].id
-      subnet_id = aws_subnet.subnet0["subnet1"].id
+    DMZ = {
+      vpc_id = aws_vpc.vpc0["central"].id
+      subnet_id = aws_subnet.subnet0["DMZ"].id
+    }
+    IB = {
+      vpc_id = aws_vpc.vpc0["central"].id
+      subnet_id = aws_subnet.subnet0["IB"].id
     }
   }
   
   route = {
-    route0 = {
-      route_table_id = "${aws_vpc.vpc0["vpc0"].default_route_table_id}"
+    DMZ_default = {
+      route_table_id = "${aws_route_table.rtb0["DMZ"].id}"
       destination_cidr_block = "0.0.0.0/0"
       gateway_id = "${aws_internet_gateway.igw0["igw0"].id}"
     }
-    route1 = {
-      route_table_id = "${aws_route_table.rtb0["rtb0"].id}"
-      destination_cidr_block = "0.0.0.0/0"
-      gateway_id = "${aws_nat_gateway.nat0["nat0"].id}"
+    DMZ_vpc1 = {
+      route_table_id = "${aws_route_table.rtb0["DMZ"].id}"
+      destination_cidr_block = "10.0.0.0/16"
+      vpc_peering_connection_id = aws_vpc_peering_connection.vpc_pc0["central_vpc1"].id
+    }
+    IB_vpc1 = {
+      route_table_id = "${aws_route_table.rtb0["IB"].id}"
+      destination_cidr_block = "10.0.0.0/16"
+      vpc_peering_connection_id = aws_vpc_peering_connection.vpc_pc0["central_vpc1"].id
     }
   }
   eip = {
     eip0 = {
-      instance = aws_instance.instance0["instance0"].id
+      instance = aws_instance.instance0["proxy"].id
       vpc = true
     }
   }
   sg = {
-    sg0 = {
-      description = "public_subnet"
-      vpc_id = aws_vpc.vpc0["vpc0"].id
+    central = {
+      description = "central"
+      vpc_id = aws_vpc.vpc0["central"].id
     }
-    sg1 = {
-      description = "private_subnet"
-      vpc_id = aws_vpc.vpc0["vpc0"].id
+    vpc1 = {
+      description = "vpc1"
+      vpc_id = aws_vpc.vpc0["vpc1"].id
     }
   }
   sgr = {
-    sg0r0 = {
-      security_group_id = "${aws_security_group.sg0["sg0"].id}"
+    central_i = {
+      security_group_id = "${aws_security_group.sg0["central"].id}"
       type = "ingress"
       from_port = 0
       to_port = 0
       protocol = "-1"
       cidr_blocks = ["0.0.0.0/0"]
     }
-    sg0r1 = {
-      security_group_id = "${aws_security_group.sg0["sg0"].id}"
+    central_e = {
+      security_group_id = "${aws_security_group.sg0["central"].id}"
       type = "egress"
       from_port = 0
       to_port = 0
       protocol = "-1"
       cidr_blocks = ["0.0.0.0/0"]
     }
-    sg1r0 = {
-      security_group_id = "${aws_security_group.sg0["sg1"].id}"
+    vpc1_i = {
+      security_group_id = "${aws_security_group.sg0["vpc1"].id}"
       type = "ingress"
       from_port = 0
       to_port = 0
       protocol = "-1"
-      source_security_group_id = "${aws_security_group.sg0["sg0"].id}"
+      cidr_blocks = ["0.0.0.0/0"]
     }
-    sg1r1 = {
-      security_group_id = "${aws_security_group.sg0["sg1"].id}"
+    vpc1_e = {
+      security_group_id = "${aws_security_group.sg0["vpc1"].id}"
       type = "egress"
       from_port = 0
       to_port = 0
       protocol = "-1"
       cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  vpc_pc = {
+    central_vpc1 = {
+      peer_owner_id = "962615889483"
+      peer_vpc_id = aws_vpc.vpc0["vpc1"].id
+      vpc_id = aws_vpc.vpc0["central"].id
+    }
+  }
+  nacl = {
+    central = {
+      vpc_id = aws_vpc.vpc0["central"].id
+      subnet_ids = [
+        aws_subnet.subnet0["DMZ"].id,
+        aws_subnet.subnet0["IB"].id
+      ]
+    }
+    vpc1 = {
+      vpc_id = aws_vpc.vpc0["vpc1"].id
+      subnet_ids = [
+        aws_subnet.subnet0["vpc1sub0"].id
+      ]
+    }
+  }
+  nacl_rule = {
+    central_i = {
+      network_acl_id = aws_network_acl.nacl0["central"].id
+      rule_number = 200
+      protocol = "-1"
+      rule_action = "allow"
+      cidr_block = "0.0.0.0/0"
+      from_port = 0
+      to_port = 0
+    }
+    central_e = {
+      network_acl_id = aws_network_acl.nacl0["central"].id
+      rule_number = 200
+      protocol = "-1"
+      rule_action = "allow"
+      cidr_block = "0.0.0.0/0"
+      egress = true
+      from_port = 0
+      to_port = 0
+    }
+    vpc1_i = {
+      network_acl_id = aws_network_acl.nacl0["vpc1"].id
+      rule_number = 200
+      protocol = "-1"
+      rule_action = "allow"
+      cidr_block = "0.0.0.0/0"
+      from_port = 0
+      to_port = 0
+    }
+    vpc1_e = {
+      network_acl_id = aws_network_acl.nacl0["vpc1"].id
+      rule_number = 200
+      protocol = "-1"
+      rule_action = "allow"
+      cidr_block = "0.0.0.0/0"
+      egress = true
+      from_port = 0
+      to_port = 0
+    }
+    central_vpc1_i = {
+      network_acl_id = aws_network_acl.nacl0["central"].id
+      rule_number = 300
+      protocol = "-1"
+      rule_action = "allow"
+      cidr_block = aws_vpc.vpc0["vpc1"].cidr_block
+      from_port = 0
+      to_port = 0
+    }
+    central_vpc1_e = {
+      network_acl_id = aws_network_acl.nacl0["central"].id
+      rule_number = 300
+      protocol = "-1"
+      egress = true
+      rule_action = "allow"
+      cidr_block = aws_vpc.vpc0["vpc1"].cidr_block
+      from_port = 0
+      to_port = 0
+    }
+    vpc1_central_i = {
+      network_acl_id = aws_network_acl.nacl0["vpc1"].id
+      rule_number = 300
+      protocol = "-1"
+      rule_action = "allow"
+      cidr_block = aws_vpc.vpc0["central"].cidr_block
+      from_port = 0
+      to_port = 0
+    }
+    vpc1_central_e = {
+      network_acl_id = aws_network_acl.nacl0["vpc1"].id
+      rule_number = 300
+      protocol = "-1"
+      egress = true
+      rule_action = "allow"
+      cidr_block = aws_vpc.vpc0["central"].cidr_block
+      from_port = 0
+      to_port = 0
     }
   }
 }
